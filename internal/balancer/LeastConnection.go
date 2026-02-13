26b5bcfc046cc5344fcf06c2334f0084cc8c3a13
patiabhishek123 <abhishekcloud2003@gmail.com>
2026-02-13 23:02:09 +0530

    least count logic

--- internal/balancer/LeastConnection.go
+++ internal/balancer/LeastConnection.go
@@ -1,2 +1,32 @@
 package balancer
 
+type leastConn struct{
+	pool *BackendPool
+}
+
+func NewLeastCount(pool *BackendPool)*leastConn{
+	return &leastConn{
+		pool: pool,
+	}
+}
+
+func (lc *leastConn) NextBackend() *Backend{
+
+	healthy:= lc.pool.GetHealthyBackends()
+
+	if len(healthy)==0 {
+		return nil
+	}
+
+	var selected *Backend
+	minConn :=int64(-1)
+	for _,b :=range healthy{
+		conn :=b.ActiveConnections
+
+		if minConn ==-1 || conn < minConn {
+			minConn = conn
+			selected=b
+		}
+	}
+	return selected
+}
